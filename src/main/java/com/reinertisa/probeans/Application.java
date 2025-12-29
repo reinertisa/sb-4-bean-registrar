9188fdcdc4f24ac0aaedf6c4d4729bbcbf4dd81e
Isa Reinert <reinert.isa@gmail.com>
2025-12-29 00:53:57 -0800

    feat(api): initialize project with new spring boot 4 bean registrar

---
+++ src/main/java/com/reinertisa/probeans/Application.java
@@ -0,0 +1,13 @@
+package com.reinertisa.probeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb4BeanRegistrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb4BeanRegistrarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reinertisa/probeans/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.probeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb4BeanRegistrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
