78777940be3c98df1718c5a861aefe9227998c04
Isa Reinert <reinert.isa@gmail.com>
2025-12-29 00:59:39 -0800

    refactor(api): rename application start class name

--- src/main/java/com/reinertisa/probeans/Application.java
+++ src/main/java/com/reinertisa/probeans/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sb4BeanRegistrarApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sb4BeanRegistrarApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
